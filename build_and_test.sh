46856019ba6d4f5fc7b7b16c3dfea30c1afe941d
e <6410762+e-mit@users.noreply.github.com>
2024-03-23 15:31:53 +0000

    Enable public dashboard and print URL

--- build_and_test.sh
+++ build_and_test.sh
@@ -8,8 +8,27 @@ docker run -td -p 3000:3000 --name test \
   -e GF_INSTALL_PLUGINS=frser-sqlite-datasource,grafana-clock-panel \
   --rm test:latest
 
+
 # do tests here. Run command like:
 # docker exec test python -m mypy . --exclude 'tests/' --exclude 'venv/'
 # docker exec test sh -c 'python -m pycodestyle *.py --exclude=tests/*,venv/*'
+
+
+# Enable public dashboard (cannot be provisioned in files) and print its URL:
+DASHBOARD_UID=bdgisvc9bvym8b  # must match dashboard json file
+_attempt_public_dashboard_enable() {
+    CURL_OUTPUT=$(curl -u admin:${GF_PASSWORD} -XPOST "http://localhost:3000/api/dashboards/uid/${DASHBOARD_UID}/public-dashboards" -H "content-type: application/json" --data-raw '{"isEnabled":true}')
+}
+_attempt_public_dashboard_enable
+while [[ "$?" -ne 0 ]]; do
+    sleep 5
+    _attempt_public_dashboard_enable
+done
+printf '%s\n' "$CURL_OUTPUT" | python3 -c \
+"import sys, json
+id = json.load(sys.stdin)['accessToken']
+print(f'http://localhost:3000/public-dashboards/{id}')"
+
+
 
 # docker stop -t 0 test
