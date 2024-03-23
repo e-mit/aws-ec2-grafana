5435087e96513cb384a50997b89f3d8338028e9b
e <6410762+e-mit@users.noreply.github.com>
2024-03-23 14:02:12 +0000

    Fix admin password setting

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-PORT=3000
-
 docker build -t test:latest .
 
 # The -td forces the container to keep running without any CMD, and as a daemon
-docker run -td -p 3000:${PORT} --name test \
-  -e PORT=${PORT} \
-  -e GF_ADMIN_PASSWORD=${GF_PASSWORD} \
-  -e "GF_INSTALL_PLUGINS=frser-sqlite-datasource,grafana-clock-panel" \
+docker run -td -p 3000:3000 --name test \
+  -e GF_SECURITY_ADMIN_PASSWORD=${GF_PASSWORD} \
+  -e GF_INSTALL_PLUGINS=frser-sqlite-datasource,grafana-clock-panel \
   --rm test:latest
 
 # do tests here. Run command like:
