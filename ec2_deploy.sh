9385783f754be63ddbce0cbd4fe2dbb4d7328027
e <6410762+e-mit@users.noreply.github.com>
2024-03-24 16:09:47 +0000

    Add docker-compose installation

--- ec2_deploy.sh
+++ ec2_deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Install docker and run a web app container on an already-existing EC2 instance.
 # Note that the EC2 instance must have a policy to allow incoming web requests.
+# Also note: docker-compose is not in the AMI dnf package repo, so is installed
+# from docker github. This means it will not be updated with dnf updates.
 
 KEY_FILENAME=../aws-create-db/key.pem
 EC2_IP=13.43.90.54
 DOCKER_CONTAINER_IMAGE="emit5/grafana-test:latest"
 # GF_SECURITY_ADMIN_PASSWORD  # define this
+DOCKER_COMPOSE_GITHUB="https://github.com/docker/compose/releases/download/v2.25.0/docker-compose-linux-x86_64"
 
 ################################################
 
@@ -25,9 +28,21 @@ ssh -t -i $KEY_FILENAME -o StrictHostKeyChecking=accept-new \
 # ssh -t -i $KEY_FILENAME -o StrictHostKeyChecking=accept-new \
 #    ec2-user@$EC2_IP ENV_VAR=$LOCAL_ENV_VAR "${SSH_SCRIPT}"
 
-
-# log back in, download the container image, and run it (with restart option):
+# log back in, download and install docker-compose (not in distro repo)
+# as described at https://docs.docker.com/compose/install/linux/#install-the-plugin-manually
 SSH_SCRIPT2="
+DOCKER_CONFIG=\${DOCKER_CONFIG:-\$HOME/.docker}
+mkdir -p \$DOCKER_CONFIG/cli-plugins
+curl -SL $DOCKER_COMPOSE_GITHUB \
+-o \$DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x \$DOCKER_CONFIG/cli-plugins/docker-compose
+"
+ssh -t -i $KEY_FILENAME -o StrictHostKeyChecking=accept-new \
+    ec2-user@$EC2_IP "${SSH_SCRIPT2}"
+
+
+# download the container image, and run it (with restart option):
+SSH_SCRIPT3="
 docker pull $DOCKER_CONTAINER_IMAGE
 docker run -d -p 3000:3000 --name grafana-test \
 -e GF_SECURITY_ADMIN_PASSWORD=$GF_SECURITY_ADMIN_PASSWORD \
@@ -35,15 +50,11 @@ docker run -d -p 3000:3000 --name grafana-test \
 --restart=always $DOCKER_CONTAINER_IMAGE
 "
 ssh -t -i $KEY_FILENAME -o StrictHostKeyChecking=accept-new \
-    ec2-user@$EC2_IP "${SSH_SCRIPT2}"
+    ec2-user@$EC2_IP "${SSH_SCRIPT3}"
 
 # NB: can still stop and delete the container (not the image) with:
 # docker stop -t 0 <name>
 # docker container rm <name>
-
-
-
-
 
 
 if (( 0==1 )); then
