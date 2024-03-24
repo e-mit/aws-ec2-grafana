ccd5fd262e8977de3cfd92a45373d9c9322723a9
e <6410762+e-mit@users.noreply.github.com>
2024-03-24 11:21:23 +0000

    Rename example settings file

--- db_settings.sh
+++
@@ -1,6 +0,0 @@
-export GF_PASSWORD=admin1234
-export TEST_DB_PORT=5432
-export TEST_DB_USER=grafanauser
-export TEST_DB_NAME=grafanatest
-export TEST_DB_HOST=host.docker.internal
-export TEST_DB_PASSWORD=Test1234
