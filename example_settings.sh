730d73618f55d10e83d307001a93501afe688b8d
e <6410762+e-mit@users.noreply.github.com>
2024-03-26 13:32:51 +0000

    Remove file

--- example_settings.sh
+++
@@ -1,6 +0,0 @@
-export DB_PORT=5432
-export DB_USER=grafanauser
-export DB_NAME=grafanatest
-export DB_HOST=host.docker.internal
-# export DB_PASSWORD  # define separately
-# export GF_PASSWORD  # define separately
