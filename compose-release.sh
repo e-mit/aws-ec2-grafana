7353b64977d17619e796f8d1fbc287d5cc260ea3
e <6410762+e-mit@users.noreply.github.com>
2024-03-26 17:24:47 +0000

    Move all environment variables into env.txt and exclude it from git and Docker

--- compose-release.sh
+++ compose-release.sh
@@ -3,18 +3,7 @@
 # Build and run the release configuration (uses
 # Postgres database)
 
-# export GF_PASSWORD  # define this
-export DASHBOARD_UID=bdgisvc9bvym8bdashboard
-export DOMAIN_NAME=localhost
-export NGINX_PORT=8080
-export DB_PORT=5432
-export DB_USER=grafanauser
-export DB_NAME=grafanatest
-export DB_HOST=host.docker.internal
-# export DB_PASSWORD  # define this
-
-
 docker build -f Dockerfile --target release -t emit5/grafana-release:latest .
 docker build -f Dockerfile-nginx -t emit5/nginx-grafana:latest .
 
-docker compose -f compose-release.yaml up --force-recreate
+docker compose -f compose-release.yaml --env-file env.txt up --force-recreate
--- compose-test.sh
+++ compose-test.sh
@@ -3,14 +3,7 @@
 # Build and run the test configuration (uses
 # SQLite database within the grafana container)
 
-# export GF_PASSWORD  # define this
-export DASHBOARD_UID=bdgisvc9bvym8bdashboard
-export DOMAIN_NAME=localhost
-export NGINX_PORT=8080
-
-###################################################
-
 docker build -f Dockerfile --target test -t emit5/grafana-test:latest .
 docker build -f Dockerfile-nginx -t emit5/nginx-grafana:latest .
 
-docker compose -f compose-test.yaml up --force-recreate
+docker compose -f compose-test.yaml --env-file env.txt up --force-recreate
