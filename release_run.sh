b5fbc2289e6f7b00a875e85d827c842b66dc13b4
e <6410762+e-mit@users.noreply.github.com>
2024-03-27 18:40:20 +0000

    Combine the two local test scripts

--- release_run.sh
+++ release_run.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-# Build and run the Grafana release container alone, and
-# enables the public dashboard.
+# Build and run the Grafana container locally
+# and enable the public dashboard.
 
+# Choose test or release:
+CONFIG=test
+# test = use SQLite database in the container
+# release = use a PostgreSQL database as specified by the following
 export DB_PORT=5432
 export DB_USER=grafanauser
 export DB_NAME=grafanatest
 export DB_HOST=host.docker.internal
-# export DB_PASSWORD  # define separately
-# export GF_PASSWORD  # define separately
+# export DB_PASSWORD  # define this
+# export GF_PASSWORD  # define this
 
-docker build --target release -t release:latest .
+########################################################################
 
-docker run -td -p 3000:3000 --name release \
+docker build --target $CONFIG -t $CONFIG:latest .
+
+docker run -td -p 3000:3000 --name $CONFIG \
   -e GF_SECURITY_ADMIN_PASSWORD=${GF_PASSWORD} \
+  -e GF_INSTALL_PLUGINS=frser-sqlite-datasource,grafana-clock-panel \
   -e DB_PORT=${DB_PORT} \
   -e DB_USER=${DB_USER} \
   -e DB_NAME=${DB_NAME} \
   -e DB_HOST=${DB_HOST} \
   -e DB_PASSWORD=${DB_PASSWORD} \
-  --rm release:latest
+  --rm $CONFIG:latest
 
 # Enable public dashboard (cannot be provisioned in files) and print its URL:
+echo ""
+echo "Waiting for Grafana to startup..."
+echo ""
 DASHBOARD_UID=bdgisvc9bvym8bdashboard  # must match dashboard json file
 _attempt_public_dashboard_enable() {
     CURL_OUTPUT=$(curl -u admin:${GF_PASSWORD} \
@@ -36,11 +46,22 @@ done
 DASH_ID=$(printf '%s\n' "$CURL_OUTPUT" | jq -r '.accessToken')
 echo "Dashboard available at: http://localhost:3000/public-dashboards/$DASH_ID"
 
-# docker stop -t 0 release
+
+if [ $CONFIG = "release" ]; then
+    # Disable the sslmode of the grafana postgres (only needed for release config)
+curl -u admin:${GF_PASSWORD} -X PUT "http://localhost:3000/api/datasources/uid/bdgisvc9bvym8brelease" \
+-H "content-type: application/json" --data-raw \
+'{"id":1,"uid":"bdgisvc9bvym8brelease","orgId":1,"name":"postgres",'\
+'"type":"grafana-postgresql-datasource","typeLogoUrl":'\
+'"public/app/plugins/datasource/grafana-postgresql-datasource/img/postgresql_logo.svg",'\
+'"access":"proxy","url":"host.docker.internal:5432","user":"grafanauser","database":"",'\
+'"basicAuth":false,"basicAuthUser":"","withCredentials":false,"isDefault":true,"jsonData":'\
+'{"connMaxLifetime":14400,"database":"grafanatest","maxOpenConns":5,"postgresVersion":1400,'\
+'"sslmode":"disable","timescaledb":false},"secureJsonFields":{"password":true},'\
+'"version":3,"readOnly":false}' &> /dev/null
+fi
 
 
-# Note on docker push
-# Always tag twice: version number and latest: then can easily pull latest
-# docker build -t reg/user/image:ver -t reg/user/image:latest .
-# (nb: can also build then tag later)
-# docker push reg/user/image --all-tags
+# Stop and remove:
+# docker stop -t 0 $CONFIG
+
