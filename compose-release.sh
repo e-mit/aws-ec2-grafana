84d4ffc4a518c262c917adce499d89604b7a0fc4
e <6410762+e-mit@users.noreply.github.com>
2024-03-27 16:44:49 +0000

    Disable caching when building images

--- compose-release.sh
+++ compose-release.sh
@@ -3,9 +3,9 @@
 # Build and run the release configuration (uses
 # Postgres database)
 
-docker build -f Dockerfile --target release -t emit5/grafana-release:latest .
-docker build -f Dockerfile-nginx -t emit5/nginx-grafana:latest .
-#docker push emit5/nginx-grafana:latest
-#docker push emit5/grafana-release:latest
+docker build -f Dockerfile --no-cache --target release -t emit5/grafana-release:latest .
+docker build -f Dockerfile-nginx --no-cache -t emit5/nginx-grafana:latest .
+docker push emit5/nginx-grafana:latest
+docker push emit5/grafana-release:latest
 
 docker compose -f compose-release.yaml --env-file env.txt up --force-recreate
--- compose-test.sh
+++ compose-test.sh
@@ -3,9 +3,9 @@
 # Build and run the test configuration (uses
 # SQLite database within the grafana container)
 
-docker build -f Dockerfile --target test -t emit5/grafana-test:latest .
-docker build -f Dockerfile-nginx -t emit5/nginx-grafana:latest .
-#docker push emit5/nginx-grafana:latest
-#docker push emit5/grafana-test:latest
+docker build -f Dockerfile --no-cache --target test -t emit5/grafana-test:latest .
+docker build -f Dockerfile-nginx --no-cache -t emit5/nginx-grafana:latest .
+docker push emit5/nginx-grafana:latest
+docker push emit5/grafana-test:latest
 
 docker compose -f compose-test.yaml --env-file env.txt up --force-recreate
