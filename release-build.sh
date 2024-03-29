b03c7f8a98e6965a232c5b9686a010b2f2dd66c2
e <6410762+e-mit@users.noreply.github.com>
2024-03-29 12:51:02 +0000

    Rename file and only build

--- release-build.sh
+++ release-build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Build and run the release configuration locally (using Postgres database)
+# Build and push the release configuration
 
 docker build -f Dockerfile --no-cache --target release -t emit5/grafana-release:latest .
 docker build -f Dockerfile-nginx --no-cache -t emit5/nginx-grafana:latest .
 docker push emit5/nginx-grafana:latest
 docker push emit5/grafana-release:latest
-
-docker compose -f compose-release.yaml --env-file env.txt up --force-recreate
